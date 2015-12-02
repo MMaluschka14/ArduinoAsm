ef310261d1d96270539fc918fed7318eb101dbc6
Roie R. Black <rblack@austincc.edu>
2015-12-02 00:29:06 -0600

    Added setup.bat for PC

---
+++ setup.bat
@@ -0,0 +1,5 @@
+copy makeincs\Makefile.win \usr\local\include
+copy makeincs\Makefile.unx \usr\local\include
+copy makeincs\Makefile.mac \usr\local\include
+copy makeincs\special_regs.inc \usr\local\include
+
